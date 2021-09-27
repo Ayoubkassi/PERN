03c3aed00ce2b5203bcc48e5b3bff1e1e659ff73
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-27 23:05:34 +0100

    Add restaurant form

--- client/src/components/AddRestaurant.component.jsx
+++ client/src/components/AddRestaurant.component.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React , { useState } from 'react';
+import axios from 'axios';
+
 
 export const AddRestaurant = () => {
+  const [name ,setName]= useState('');
+  const [location , setLocation]= useState('');
+  const [price , setPrice] = useState(null);
+
+  const IncludeRestaurant = () => {
+    axios.post('/api/v1/restaurants', {
+    name: name,
+    location: location,
+    price : price
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  }
+
   return(
     <div className="mb-4">
       <form>
         <div className="form-row">
           <div className="col">
-            <input type="text" className="form-control" placeholder="Name" />
+            <input type="text" value={name} className="form-control" onChange={(e)=> setName(e.target.value)} placeholder="Name" />
           </div>
           <div className="col">
-            <input type="text" className="form-control" placeholder="Location"/>
+            <input type="text" value={location} className="form-control" onChange={(e)=> setLocation(e.target.value)} placeholder="Location"/>
           </div>
           <div className="col">
-            <select className="custom-select my-1 mr-sm-2">
+            <select className="custom-select my-1 mr-sm-2" onChange={(e) => setPrice(e.target.value)} value={price}>
               <option disbled>Price Range</option>
-              <option value="$"></option>
-              <option value="$$"></option>
-              <option value="$$$"></option>
-              <option value="$$$$"></option>
-              <option value="$$$$$"></option>
+              <option value="1">$</option>
+              <option value="2">$$</option>
+              <option value="3">$$$</option>
+              <option value="4">$$$$</option>
+              <option value="5">$$$$$</option>
             </select>
           </div>
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" type="submit" onClick={IncludeRestaurant}>Add</button>
         </div>
       </form>
     </div>
