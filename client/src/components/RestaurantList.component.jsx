f97391d1a0bd45a959186b68e2718e56df23b441
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-27 22:06:10 +0100

    Fetching Data

--- client/src/components/RestaurantList.component.jsx
+++ client/src/components/RestaurantList.component.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React , { useState , useEffect } from 'react';
+import axios from 'axios';
 
 export const RestaurantList = () => {
-  return(
 
+  const [data , setData] = useState([]);
+
+  const getRestaurants = async () => {
+      try{
+        const res = await axios.get('/api/v1/restaurants');
+        setData(res.data.data.restaurants);
+      }catch(err){
+        console.log(err)
+      }
+  }
+  useEffect(()=>{
+    getRestaurants();
+  },[])
+  return(
+    <table className="table table-dark" style={{border : 'none'}}>
+      <thead style={{backgroundColor : '#007bff' , border : 'none' , outline : 'none'}}>
+        <tr>
+          <th>Restaurant</th>
+          <th>Location</th>
+          <th>Price Range</th>
+          <th>Rating</th>
+          <th>Update</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data && data.map(restaurant => (
+            <tr>
+              <td>{restaurant.name}</td>
+              <td>{restaurant.location}</td>
+              <td>{restaurant.price}</td>
+              <td>5</td>
+              <td><button className="btn btn-success">Update</button></td>
+              <td><button className="btn btn-danger">Delete</button></td>
+
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   );
 }
--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Header } from '../../components/Header.component';
 import { AddRestaurant } from '../../components/AddRestaurant.component';
+import { RestaurantList  } from '../../components/RestaurantList.component';
+
+
 export const HomePage = () => {
   return(
     <div className="mr-4 ml-4 mt-4">
       <Header />
       <AddRestaurant />
+      <RestaurantList  />
     </div>
   );
 }
