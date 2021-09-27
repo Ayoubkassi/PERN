9ad9e3970fb4bd7c56200a3c8e4a6e359d397112
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-27 20:07:00 +0100

    Setup our application Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import { Button } from './App.styles.js';
+import { HomePage } from './pages/homepage/HomePage';
+import { RestaurantDetailPage } from './pages/RestaurantDetailPage/Restaurants';
+import { UpdateRestaurantPage } from './pages/UpdateRestaurantPage/UpdateRestaurantPage';
+import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey and Welcome Again</h1>
-      <Button color="rgb(252, 186, 3)" className="btn">
-        Click Me
-      </Button>
+    <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ HomePage } />
+            <Route path="/restaurants/:id/update" component={ UpdateRestaurantPage } />
+            <Route path="/restaurants/:id" component={ RestaurantDetailPage } />
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ client/src/pages/restaurantDetailPage/RestaurantDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const RestaurantDetailPage = () => {
+  return(
+    <h1>Detail</h1>
+  );
+}
