7f1a77ef43c6a0a90bae444c8651471b9f82b5af
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-29 16:37:51 +0100

    Set up detail page

--- client/src/pages/restaurantDetailPage/RestaurantDetailPage.jsx
+++ client/src/pages/restaurantDetailPage/RestaurantDetailPage.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React , { useEffect , useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { Header } from '../../components/Header.component';
+
 
 export const RestaurantDetailPage = () => {
+
+  const [restaurant , setRestaurant] = useState(null);
+  const [imgSrc, setImgSrc ] = useState('https://spoonacular.com/recipeImages/16682-556x370.jpg')
+  const history = useHistory();
+  const path = history.location.pathname;
+  const id = path.split('/')[2];
+  console.log(id);
+
+  const getRestaurant = async () => {
+    try{
+      const res = await axios.get(`/api/v1/restaurants/${id}`)
+      setRestaurant(res.data.data.restaurant[0]);
+      setImgSrc(`https://spoonacular.com/recipeImages/${restaurant.id}-556x370.jpg`)
+    }catch(err){
+      console.log(err);
+    }
+  }
+  useEffect(()=> {
+    getRestaurant();
+  },[restaurant]);
+
   return(
-    <h1>Detail</h1>
+    <div style={{backgroundColor: 'black', color : 'white', height : '100vh'}}>
+      <div className="container"  >
+        <Header text={restaurant && restaurant.name } />
+        <h2 className="text-center mb-4">Location : { restaurant && restaurant.location }</h2>
+        <img src={imgSrc && imgSrc} alt="Food" style={{marginLeft : '18%'}}  />
+      </div>
+    </div>
   );
 }
